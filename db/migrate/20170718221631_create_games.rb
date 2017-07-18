db07a8a7c66a20af049cc4996298e3643bed3141
Paola Guitian <poliguitian@gmail.com>
2017-07-18 18:41:54 -0400

    master

---
+++ db/migrate/20170718221631_create_games.rb
@@ -0,0 +1,15 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.string :location
+      t.string :address
+      t.datetime :start
+      t.datetime :stop
+      t.string :name
+      t.string :sport
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/controllers/games_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class GamesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @game = games(:one)
+  end
+
+  test "should get index" do
+    get games_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_game_url
+    assert_response :success
+  end
+
+  test "should create game" do
+    assert_difference('Game.count') do
+      post games_url, params: { game: { address: @game.address, description: @game.description, location: @game.location, name: @game.name, sport: @game.sport, start: @game.start, stop: @game.stop } }
+    end
+
+    assert_redirected_to game_url(Game.last)
+  end
+
+  test "should show game" do
+    get game_url(@game)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_game_url(@game)
+    assert_response :success
+  end
+
+  test "should update game" do
+    patch game_url(@game), params: { game: { address: @game.address, description: @game.description, location: @game.location, name: @game.name, sport: @game.sport, start: @game.start, stop: @game.stop } }
+    assert_redirected_to game_url(@game)
+  end
+
+  test "should destroy game" do
+    assert_difference('Game.count', -1) do
+      delete game_url(@game)
+    end
+
+    assert_redirected_to games_url
+  end
+end
