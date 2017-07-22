32273212f10b1bf03755ce857ab95c8b0059b952
Jason Hertzbach <hertzbachj@gmail.com>
2017-07-22 13:23:20 -0400

    not 100% sure what changed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to games_path
     else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      # If user's login doesn't work, send them back to the login form.
+      redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to login_path
   end
 end
