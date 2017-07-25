98d2241c2e2dcf0f4de3a2f5bd1fbbf81c068d5b
Paola Guitian <poliguitian@gmail.com>
2017-07-25 18:33:49 -0400

    create game validations and description placeholder

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,15 @@ class Game < ApplicationRecord
   has_many :users, through: :game_players
   geocoded_by :location   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
+  validates :location, presence: true
+  validates :sport, presence: true
+  validates :start, presence: true
+  validates :description, presence: true
 
+
+
+
+#search method for search ability
   def self.search(search)
     where("location LIKE ? OR description LIKE ? OR sport LIKE ?",
       "%#{search}%", "%#{search}%", "%#{search}%")
